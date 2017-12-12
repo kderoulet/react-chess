6ec27fcfdbc97e4a05c0bba898d171a70e90065b
Kevin de Roulet <kderoulet@yahoo.com>
2017-12-11 23:04:29 -0800

    fixes piece rendering

--- src/components/BoardSquare.js
+++ src/components/BoardSquare.js
@@ -5,27 +5,51 @@ const BoardSquare = (props) => {
     switch (props.squareVal) {
         case 1: content = "♙";
         break
+        case 101: content = "♙";
+        break
         case 2: content = "♟";
+        break
+        case 102: content = "♟";
         break
         case 3: content = "♗";
         break
+        case 103: content = "♗"
+        break
         case 4: content = "♝";
+        break
+        case 104: content = "♝"
         break
         case 5: content = "♘";
         break
+        case 105: content = "♘"
+        break
         case 6: content = "♞";
+        break
+        case 106: content = "♞";
         break
         case 7: content = "♖";
         break
+        case 107: content = "♖";
+        break
         case 8: content = "♜";
+        break
+        case 108: content = "♜";
         break
         case 9: content = "♕";
         break
+        case 109: content = "♕";
+        break
         case 10: content = "♛";
+        break
+        case 110: content = "♛";
         break
         case 11: content = "♔";
         break
+        case 111: content = "♔";
+        break
         case 12: content = "♚";
+        break
+        case 112: content = "♚";
         break
         default: break;
         
